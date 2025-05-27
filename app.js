5a8febbb9140973bf389c8e856cab6bba0b9af03
bentleyminwa <bentleyminwa@gmail.com>
2025-05-27 22:27:52 +0300

    Refactor task deletion to use SVG icon and update styles for improved theming

--- app.js
+++ app.js
@@ -12,11 +12,29 @@ function addTask(e) {
   taskList.appendChild(li);
   taskInput.value = "";
 
-  const deleteIcon = document.createElement("img");
-  deleteIcon.classList.add("delete-icon");
-  deleteIcon.src = "images/trash.png";
+  // const deleteIcon = document.createElement("img");
+  // deleteIcon.classList.add("delete-icon");
+  // deleteIcon.src = "images/trash.png";
 
-  li.appendChild(deleteIcon);
+  // trash icon
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  svg.setAttribute("width", "24");
+  svg.setAttribute("height", "24");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("fill", "none");
+  svg.setAttribute("stroke", "currentColor");
+  svg.setAttribute("stroke-width", "2");
+  svg.setAttribute("stroke-linecap", "round");
+  svg.setAttribute("stroke-linejoin", "round");
+
+  svg.innerHTML = `
+    <path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/>
+  `;
+
+  svg.classList.add("delete-icon");
+
+  li.appendChild(svg);
   saveTask();
 }
 
@@ -26,7 +44,7 @@ taskList.addEventListener(
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("completed");
       saveTask();
-    } else if (e.target.tagName === "IMG") {
+    } else if (e.target.tagName === "svg") {
       e.target.parentElement.remove();
       saveTask();
     }
